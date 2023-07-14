14a72f74684375f72c800542cddf178d33600164
Alptekin <alptekin.ocakdan@gmail.com>
2023-07-14 12:11:34 +0300

    imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Topbar from "./components/Topbar/Topbar.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Categories from "./components/Categories/Categories.jsx";
+import Grid from "./components/Grid/Grid.jsx";
+import Products from "./components/Products/Products.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
