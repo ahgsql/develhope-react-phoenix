22e307c98a598bbf9265db83b3876f9ed0dcfc31
ahgsql <35695543+ahgsql@users.noreply.github.com>
2023-07-17 15:10:54 +0300

    Topbar, Product, search functionality, clickoutside hook

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,5 +1,55 @@
-import React from "react";
-
+import React, { useState, useRef, useEffect } from "react";
+import "./Topbar.css";
+import logo from "../../assets/logo.png";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import products from "./products";
+import InputWithIcon from "./InputWithIcon.jsx";
+import RightNavButtons from "./RightNavButtons";
+import ProductSearchResult from "./ProductSearchResult";
+import clickOutside from "../../hooks/useClickOutside";
+const changeTheme = () => {
+  console.log("Theme Changing");
+};
 export default function Topbar() {
-  return <div>Topbar</div>;
+  const [search, setSearch] = useState("");
+  const [searchResultsOpen, setSearchResultsOpen] = useState(false);
+  const resultRef = useRef();
+  clickOutside(resultRef, () => setSearchResultsOpen(false));
+  const searchList = products
+    .filter((product) => {
+      return (
+        product.title
+          .toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase()) ||
+        product.description
+          .toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase())
+      );
+    })
+    .map((product) => (
+      <ProductSearchResult key={product.id} product={product} />
+    ));
+  return (
+    <section className="topbar">
+      <div className="logo-container">
+        <img className="logo" src={logo}></img>
+        <span>{"phoneix"}</span>
+      </div>
+      <div className="search" ref={resultRef}>
+        <InputWithIcon
+          iconname={faMagnifyingGlass}
+          setSearch={setSearch}
+          setSearchResultsOpen={setSearchResultsOpen}
+        />
+        {search.length > 0 && searchResultsOpen && (
+          <div className="searchResult">
+            {searchList.length > 0 ? searchList : "No Results"}
+          </div>
+        )}
+      </div>
+      <div>
+        <RightNavButtons changeTheme={changeTheme} />
+      </div>
+    </section>
+  );
 }
---
+++ src/components/Topbar/products.js
@@ -0,0 +1,122 @@
+export default [
+  {
+    id: 1,
+    title: "Acme Home, Blanket",
+    description:
+      "A cozy and warm blanket for cold nights. This blanket is made of high-quality material and provides excellent comfort.",
+    price: 100,
+    photo: "https://picsum.photos/id/1/60",
+  },
+  {
+    id: 2,
+    title: "Casa Deco, Chair",
+    description:
+      "A comfortable chair for your living room. It features a modern design and ergonomic support for long sitting sessions.",
+    photo: "https://picsum.photos/id/3/60",
+    price: 50,
+  },
+  {
+    id: 3,
+    title: "EcoWood, Table",
+    description:
+      "A sturdy table for various uses. This table is perfect for dining, working, or as a decorative piece in your home.",
+    photo: "https://picsum.photos/id/2/60",
+    price: 200,
+  },
+  {
+    id: 4,
+    title: "LuxeLamp, Lamp",
+    description:
+      "A stylish lamp to brighten up any room. The lamp's warm glow creates a cozy atmosphere.",
+    photo: "https://picsum.photos/id/5/60",
+    price: 30,
+  },
+  {
+    id: 5,
+    title: "ComfortZone, Cushion",
+    description:
+      "Soft and fluffy cushion for your couch or bed. It comes in various colors to match your interior.",
+    photo: "https://picsum.photos/id/6/60",
+    price: 20,
+  },
+  {
+    id: 6,
+    title: "Chic Reflections, Mirror",
+    description:
+      "A decorative mirror to add a touch of elegance to your living space. Its sleek design complements any decor.",
+    photo: "https://picsum.photos/id/7/60",
+    price: 80,
+  },
+  {
+    id: 7,
+    title: "BookWorm, Bookshelf",
+    description:
+      "A spacious bookshelf to organize your books and display your favorite items. It's both functional and visually appealing.",
+    photo: "https://picsum.photos/id/8/60",
+    price: 150,
+  },
+  {
+    id: 8,
+    title: "SoftStep, Rug",
+    description:
+      "A soft and durable rug that enhances the comfort of your room while protecting your floors.",
+    photo: "https://picsum.photos/id/9/60",
+    price: 70,
+  },
+  {
+    id: 9,
+    title: "Timeless Classics, Clock",
+    description:
+      "A stylish wall clock that adds a contemporary touch to your home decor.",
+    photo: "https://picsum.photos/id/10/60",
+    price: 25,
+  },
+  {
+    id: 10,
+    title: "Bloom & Bliss, Vase",
+    description:
+      "An elegant vase to display your fresh flowers or artificial arrangements.",
+    photo: "https://picsum.photos/id/11/60",
+    price: 15,
+  },
+  {
+    id: 11,
+    title: "Craftsman's Desk, Desk",
+    description:
+      "A spacious and sturdy desk for your office or study room. It provides ample space for your work essentials.",
+    photo: "https://picsum.photos/id/12/60",
+    price: 180,
+  },
+  {
+    id: 12,
+    title: "Elegance Drapes, Curtains",
+    description:
+      "High-quality curtains that block out light and provide privacy. They come in various sizes and colors.",
+    photo: "https://picsum.photos/id/13/60",
+    price: 40,
+  },
+  {
+    id: 13,
+    title: "SofaSense, Sofa",
+    description:
+      "A comfortable and stylish sofa for your living room. It's designed to accommodate multiple people.",
+    photo: "https://picsum.photos/id/14/60",
+    price: 350,
+  },
+  {
+    id: 14,
+    title: "Urban Side, Side Table",
+    description:
+      "A compact side table that complements your seating area. It provides a convenient surface for drinks and snacks.",
+    photo: "https://picsum.photos/id/15/60",
+    price: 60,
+  },
+  {
+    id: 15,
+    title: "Home Harmony, Shelf",
+    description:
+      "A versatile shelf that can be used in the living room, bedroom, or hallway to display your favorite items.",
+    photo: "https://picsum.photos/id/16/60",
+    price: 120,
+  },
+];
