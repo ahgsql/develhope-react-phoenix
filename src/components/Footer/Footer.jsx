948b404d6f924d1d465ab9d81d76ba07081944e8
Oaks93 <16gcarlos.93@gmail.com>
2023-07-18 19:53:15 +0100

    footer first part

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,18 @@
 import React from "react";
-
+import light_30 from "./assets/light_30.png";
+import footer from "./footer.css"
 export default function Footer() {
-  return <div>Footer</div>;
+
+  return (
+    <>
+      <div className="contenedor">
+        <img width={"305px"} height={"250px"} src={light_30} alt="" />
+        <div className="texto">
+          <h3>Want to have the <strong>ultimate customer experience?</strong> </h3>
+          <h1>Become a <span style={{ color: "blue" }}>member</span> today!</h1>
+          <button style={{ backgroundColor: "blue", borderRadius: "5px" }}>Sing up </button>
+        </div>
+      </div>
+    </>
+  );
 }
