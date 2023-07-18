2c16e01eb7051213e31486cea1b41b6f0b9e9684
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-07-18 16:14:49 +0300

    button done

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { Children, useState } from "react";
+import useColorLuminance from "../../hooks/useColorLuminance";
 
-export default function Button() {
-  return <div>Button</div>;
+export default function Button({ radius = 40, bgColor = "#ffa500", children }) {
+  const [isHovered, setIsHovered] = useState(false);
+  var bgHoverColor = useColorLuminance(bgColor, -0.2);
+  const buttonStyle = {
+    borderRadius: `${radius}px`,
+    backgroundColor: `${isHovered ? bgHoverColor : bgColor}`,
+    color: "white",
+    fontSize: "16px",
+    textAlign: "center",
+    cursor: "pointer",
+    padding: "10px 20px",
+    border: "none",
+    outline: "none",
+    transition: "all 0.2s ease-in-out",
+  };
+
+  return (
+    <button
+      style={buttonStyle}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {children}
+    </button>
+  );
 }
