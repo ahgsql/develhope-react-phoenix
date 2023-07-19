36008745ea23bf97cbf7156dbc8700069ed202b8
Alptekin <alptekin.ocakdan@gmail.com>
2023-07-19 08:48:39 +0300

    Update ProductCard.jsx

--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -1,22 +1,23 @@
-const ProductCard = () => {
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+const ProductCard = (props) => {
     return (
         <div className="productCard">
             <div className="productImage">
                 <button className="wishlistButton"></button>
-                <img src="#" alt=""/>
+                <img src={props.photoFull} alt=""/>
             </div>
             <div className="productDescription">
-                <h4 className="productName"><a href="#"></a></h4>
+                <h4 className="productName"><a href="#">{props.title}</a></h4>
                 <div className="productRating">
-                    <svg height="25" width="23" className="star rating" data-rating="1">
-                        <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"/>
-                    </svg>
-                    <span className="text-muted ratingCount">(67 people rated)</span>
+                    {[...Array(props.rating)].map((index) => (
+                        <FontAwesomeIcon id={index + 1} key={index} icon="fa-solid fa-star"/>
+                    ))}
                 </div>
-                <p className="extraKnowledge">dbrand skin available</p>
-                <span className="text-muted oldProductPrice"></span>
-                <h3 className="productPrice"></h3>
-                <span className="text-muted colorCount">2 colors</span>
+                <p className="extraKnowledge">{props.eKnowledge}</p>
+                <span className="text-muted oldProductPrice">{props.oldPrice}</span>
+                <h3 className="productPrice">{props.price}</h3>
+                <span className="text-muted colorCount">{props.color}</span>
             </div>
         </div>
     );
