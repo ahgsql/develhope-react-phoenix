726d18a1cd916de31755bdfcd84e3467d29d03e2
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-07-21 10:45:33 +0300

    removed unnecessary code

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,4 +1,4 @@
-import React, { Children, useState } from "react";
+import React, { useState } from "react";
 import useColorLuminance from "../../hooks/useColorLuminance";
 import "./common.css";
 
