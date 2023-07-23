0d89b14b6b94f61847ad6acf4a9e28cd12074424
ahgsql <35695543+ahgsql@users.noreply.github.com>
2023-07-23 13:01:17 +0300

    Fixes

--- src/components/Footer/ColumnContainer.jsx
+++ src/components/Footer/ColumnContainer.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Column from "./Column";
 import logo from "./assets/logo.png";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 export default function ColumnContainer() {
   const column1Title = "About Phoenix";
   const column1Items = [
@@ -11,7 +12,14 @@ export default function ColumnContainer() {
     "Terms & Conditions",
   ];
   const column2Title = "Stay Connected";
-  const column2Items = ["Blogs", "Facebook", "Twitter"];
+  const column2Items = [
+    <li style={{ fontSize: "13px" }}>Blog</li>,
+    <li style={{ fontSize: "13px" }}>
+      <FontAwesomeIcon icon={faFacebook} style={{ color: "blue" }} />
+      Facebook
+    </li>,
+    <li style={{ fontSize: "13px" }}>Twitter</li>,
+  ];
   const column3Title = "Customer Service";
   const column3Items = ["Help Desk", "Support, 24/7", "Community of Phoenix"];
   const column4Title = "Payment Method";
