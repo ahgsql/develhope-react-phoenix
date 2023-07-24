64805a3d5be79a7deef7932bf9359c4c61239304
Oaks93 <16gcarlos.93@gmail.com>
2023-07-24 19:35:41 +0100

    button handdler

--- src/components/Footer/SignUpForm.jsx
+++ src/components/Footer/SignUpForm.jsx
@@ -4,16 +4,21 @@ import Button from '../common/Button'
 import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
-
 function SignUpForm() {
+    const handleGoogleLogin = () => {
+        window.location.href = 'https://accounts.google.com/login';
+      };
+    
+      const handleFacebookLogin = () => {
+        window.location.href = 'https://www.facebook.com/login';
+      };
     return (
         <div className='signUp'>
             <img src={logo} alt="" />
             <h3>Sign Up</h3>
             <p>Create your account today</p>
-            <button className='grey-button'><FontAwesomeIcon icon={faGoogle} style={{ color: "#e70d0d", marginRight: "5px" }} /> Sign up with Google</button>
-            <button className='grey-button'> <FontAwesomeIcon icon={faFacebook} size='lg' style={{ color: "#2760aa", marginRight: "5px" }} />  Sign up with Facebook </button>
+            <button onClick={handleGoogleLogin}><FontAwesomeIcon icon={faGoogle} style={{ color: "#e70d0d", marginRight: "5px" }} /> Sign up with Google</button>
+            <button  onClick={handleFacebookLogin}> <FontAwesomeIcon icon={faFacebook} size='lg' style={{ color: "#2760aa", marginRight: "5px" }} />  Sign up with Facebook </button>
             <form className='userCreator'>
                 <label htmlFor="name">Name</label>
                 <input type="text" name="name" id="" />
