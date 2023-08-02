886115b541dcca3ad2a7274bef4fefbac9e78c28
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-08-02 19:48:58 +0300

    icon color fix

--- src/components/ProductDetail/ProductRate.jsx
+++ src/components/ProductDetail/ProductRate.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./ProductDetail.css";
 
 export default function ProductRate({ rate }) {
   const arr = [];
   for (let i = 0; i < rate; i++) {
-    arr.push(<FontAwesomeIcon icon={faStar} />);
+    arr.push(
+      <FontAwesomeIcon
+        icon={faStar}
+        className="faStar"
+        style={{ color: "orange !important" }}
+      />
+    );
   }
 
   return <div> {arr} </div>;
