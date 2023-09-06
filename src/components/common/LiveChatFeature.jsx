aa1e90c0ad0b1ce67f8452cc8b7b68ac6aafdab5
ahgsql <35695543+ahgsql@users.noreply.github.com>
2023-09-06 13:13:30 +0300

    changes

--- src/components/common/LiveChatFeature.jsx
+++ src/components/common/LiveChatFeature.jsx
@@ -1,57 +1,106 @@
-import React, { useEffect, useState } from "react";
-import "./chatArea.css"
+import React, { useEffect, useState, useRef } from "react";
+import "./chatArea.css";
 import axios from "axios";
-import Pusher from 'pusher-js'
-
-
+import Pusher from "pusher-js";
+import Button from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 export default function LiveChatFeature() {
-    const [history, setHistory] = useState([])
-    const [message, setMessage] = useState("")
-    let bind = false
-    useEffect(() => {
-        const pusher = new Pusher('cc9492824097dabab1e1', {
-            cluster: 'eu'
-        });
-        const channel = pusher.subscribe('liveChat')
-        if (!bind) {
-            channel.bind('admin-message', function (data) {
-                setHistory(prevhistory => [...prevhistory, { from: "admin", message: data.message }])
-            });
-            bind = true
-        }
-    }, [])
-
-
-    const sendMessage = async () => {
-        let response = await axios.post(import.meta.env.VITE_BASE_URL + "/api/chat/message", { from: "customer", message })
-        if (response.data.status && message.trim().length > 0) {
-            setHistory(prevhistory => [...prevhistory, { from: "customer", message }])
-            setMessage("")
-        }
-
-
-
+  const [history, setHistory] = useState([]);
+  const [message, setMessage] = useState("");
+  let bind = false;
+  let historyRef = useRef();
+  useEffect(() => {
+    const pusher = new Pusher("cc9492824097dabab1e1", {
+      cluster: "eu",
+    });
+    const channel = pusher.subscribe("liveChat");
+    if (!bind) {
+      channel.bind("admin-message", function (data) {
+        setHistory((prevhistory) => [
+          ...prevhistory,
+          { from: "admin", message: data.message },
+        ]);
+        scrollToLast();
+      });
+      bind = true;
     }
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            e.preventDefault(); 
-            sendMessage();
-        }
-    }
-    return (
-        <>
-            <div className="chat-area">
-                <div className="chat-history">
-                    {history.map((message, i) => {
-                        return <div className={message.from + "-msg message"} key={i}> {message.from}:{message.message}</div>
-                    })}
-                </div>
-                <div className="chat-input">
-                    <input type="text" className="message-input" value={message} onChange={(e) => setMessage(e.target.value)} onKeyDown={handleKeyDown} />
-                    <button onClick={sendMessage}>send</button>
-                </div>
-            </div>
+  }, []);
 
-        </>
-    )
+  const scrollToLast = () => {
+    const scrollHeight = historyRef.current?.scrollHeight;
+    const height = historyRef.current?.clientHeight;
+    const maxScrollTop = scrollHeight - height;
+    historyRef.current.scrollTop = historyRef.current?.scrollHeight + 1000;
+  };
+
+  const sendMessage = async () => {
+    let response = await axios.post(
+      import.meta.env.VITE_BASE_URL + "/api/chat/message",
+      { from: "customer", message }
+    );
+    if (response.data.status && message.trim().length > 0) {
+      setHistory((prevhistory) => [
+        ...prevhistory,
+        { from: "customer", message },
+      ]);
+      setMessage("");
+      scrollToLast();
+    }
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      sendMessage();
+    }
+  };
+  return (
+    <>
+      <div className="chat-area">
+        <div className="contact bar">
+          <div className="pic stark"></div>
+          <div className="name">Customer Representative</div>
+        </div>
+        <div className="chat-history" ref={historyRef}>
+          {history.map((message, i) => {
+            return (
+              <div className={message.from + "-msg message"} key={i}>
+                {" "}
+                {message.from}:{message.message}
+              </div>
+            );
+          })}
+        </div>
+        <div
+          className="chat-input"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            padding: "10px",
+            alignContent: "baseline",
+          }}
+        >
+          <input
+            type="text"
+            className="message-input"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <Button
+            bgColor="#7fe1d9"
+            style={{
+              maxWidth: "90px",
+              minWidth: "50px",
+              borderRadius: "5px",
+              padding: 0,
+            }}
+            onClick={sendMessage}
+          >
+            <FontAwesomeIcon icon={faPaperPlane} />
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
