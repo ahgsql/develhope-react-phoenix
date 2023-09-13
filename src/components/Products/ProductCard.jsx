cb2c7a862be352ab9b2398db2fad39f88e9f35ca
ahgsql <35695543+ahgsql@users.noreply.github.com>
2023-09-14 01:06:28 +0300

    What is Happening

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,72 +1,91 @@
 import PropTypes from "prop-types";
 
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faHeart as faHeartRegular} from "@fortawesome/free-regular-svg-icons";
-import {faHeart as faHeartSolid} from "@fortawesome/free-solid-svg-icons";
-import {useState} from "react";
-import {Link} from "react-router-dom";
-import {Rating} from "react-simple-star-rating";
-import {useWishlist} from "../../context/WishlistProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Rating } from "react-simple-star-rating";
+import { useWishlist } from "../../context/WishlistProvider";
 
 const ProductCard = ({
-                         id,
-                         slug,
-                         photoFull,
-                         title,
-                         rating,
-                         eKnowledge,
-                         oldPrice,
-                         price,
-                         color,
-                         ratedPeople,
-                     }) => {
-    const [isHovered, setIsHovered] = useState(false);
-    const [className, setClassName] = useState("wishlistButton");
-    const {addProductToWishlist} = useWishlist();
-
-    return (
-        <div className="productCard">
-            <button
-                className={className}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-                onClick={() => addProductToWishlist(id)}
-            >
-                <FontAwesomeIcon icon={isHovered ? faHeartSolid : faHeartRegular}/>
-            </button>
-            <Link to={"/product/" + slug} style={{textDecoration: "none"}}>
-                <div className="productImage">
-                    <img src={photoFull} alt=""/>
-                </div>
-                <div className="productDescription">
-                    <h4 className="productName">
-                        <span style={{fontWeight: "bold"}}>{title}</span>
-                    </h4>
-                    <div className="productRating">
-                        <Rating initialValue={rating} readonly={true} size={20}/>
-                        <span>({ratedPeople} people rated)</span>
-                    </div>
-                    <p className="extraKnowledge">{eKnowledge}</p>
-                    <div className="prices">
-                        <span className="text-muted oldProductPrice">${oldPrice}</span>
-                        <span className="productPrice">${price}</span>
-                    </div>
-                    <span className="text-muted colorCount">{color} colors</span>
-                </div>
-            </Link>
+  id,
+  slug,
+  photoFull,
+  title,
+  rating,
+  eKnowledge,
+  oldPrice,
+  price,
+  color,
+  ratedPeople,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isInWishList, setIsInWishList] = useState(false);
+  const [className, setClassName] = useState("wishlistButton");
+  const { addProductToWishlist, wishlist, removeProductFromWishlist } =
+    useWishlist();
+  useEffect(() => {
+    if (wishlist?.products?.length > 0) {
+      if (wishlist.products.filter((p) => p.id == id).length > 0) {
+        setClassName("clickedWishlistButton");
+        setIsInWishList(true);
+      }
+    }
+  }, [wishlist]);
+  return (
+    <div className="productCard">
+      <button
+        className={className}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={() => {
+          if (isInWishList) {
+            removeProductFromWishlist(id);
+            setClassName("wishlistButton");
+            setIsInWishList(false);
+          } else {
+            addProductToWishlist(id);
+          }
+        }}
+      >
+        <FontAwesomeIcon
+          icon={isHovered || isInWishList ? faHeartSolid : faHeartRegular}
+        />
+      </button>
+      <Link to={"/product/" + slug} style={{ textDecoration: "none" }}>
+        <div className="productImage">
+          <img src={photoFull} alt="" />
         </div>
-    );
+        <div className="productDescription">
+          <h4 className="productName">
+            <span style={{ fontWeight: "bold" }}>{title}</span>
+          </h4>
+          <div className="productRating">
+            <Rating initialValue={rating} readonly={true} size={20} />
+            <span>({ratedPeople} people rated)</span>
+          </div>
+          <p className="extraKnowledge">{eKnowledge}</p>
+          <div className="prices">
+            <span className="text-muted oldProductPrice">${oldPrice}</span>
+            <span className="productPrice">${price}</span>
+          </div>
+          <span className="text-muted colorCount">{color} colors</span>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 ProductCard.propTypes = {
-    photoFull: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
-    eKnowledge: PropTypes.string.isRequired,
-    oldPrice: PropTypes.number.isRequired,
-    price: PropTypes.number.isRequired,
-    color: PropTypes.number.isRequired,
-    ratedPeople: PropTypes.number.isRequired,
+  photoFull: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  eKnowledge: PropTypes.string.isRequired,
+  oldPrice: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  color: PropTypes.number.isRequired,
+  ratedPeople: PropTypes.number.isRequired,
 };
 
 export default ProductCard;
--- src/components/Topbar/RightNavButtons.jsx
+++ src/components/Topbar/RightNavButtons.jsx
@@ -1,94 +1,94 @@
-import React, {useContext, useState, useRef} from "react";
+import React, { useContext, useState, useRef } from "react";
 import IconButton from "./IconButton";
 import {
-    faBell,
-    faHeart,
-    faSun,
-    faUser,
+  faBell,
+  faHeart,
+  faSun,
+  faUser,
 } from "@fortawesome/free-regular-svg-icons";
 import WishlitItem from "./WishlitItem";
 import useClickOutside from "../../hooks/useClickOutside";
 import Button from "../common/Button";
-import {useAuth} from "../../context/AuthProvider";
+import { useAuth } from "../../context/AuthProvider";
 
 import SignInModal from "./SignInModal";
 import Cart from "./Cart";
 import {
-    faShoppingBasket,
-    faShoppingCart,
+  faShoppingBasket,
+  faShoppingCart,
 } from "@fortawesome/free-solid-svg-icons";
-import {useCart} from "../../context/CartProvider";
-import {useWishlist} from "../../context/WishlistProvider";
+import { useCart } from "../../context/CartProvider";
+import { useWishlist } from "../../context/WishlistProvider";
 
+export default function RightNavButtons({ changeTheme }) {
+  const { user, login, logout, loginCheck } = useAuth();
+  const [wishListOpen, setWishListOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const wishlistResultDivRef = useRef();
+  const { wishlist } = useWishlist();
 
-export default function RightNavButtons({changeTheme}) {
-    const {user, login, logout, loginCheck} = useAuth();
-    const [wishListOpen, setWishListOpen] = useState(false);
-    const [modalOpen, setModalOpen] = useState(false);
-    const wishlistResultDivRef = useRef();
-    const {wishlist} = useWishlist();
+  useClickOutside(wishlistResultDivRef, () => setWishListOpen(false));
+  const searchResultDivRef = useRef();
+  useClickOutside(searchResultDivRef, () => toggleCartOpen(false));
+  const { cartItems, toggleCartOpen, isCartOpen, removeProductFromCart } =
+    useCart();
 
-    useClickOutside(wishlistResultDivRef, () => setWishListOpen(false));
-    const searchResultDivRef = useRef();
-    useClickOutside(searchResultDivRef, () => toggleCartOpen(false));
-    const {cartItems, toggleCartOpen, isCartOpen, removeProductFromCart} =
-        useCart();
-    // let WishListItems = null;
-    //
-    // if (wishlist) {
-    //     WishListItems = wishlist.map((product) => (
-    //         <WishlitItem
-    //             key={product.products.id}
-    //             id={product.products.id}
-    //             title={product.products.title}
-    //         />
-    //     ));
-    // }
+  let WishListItems = [];
 
-    return (
-        <>
-            <IconButton
-                iconname={faSun}
-                className="circle bgOrange hover"
-                onClick={() => changeTheme()}
-            />
-            <IconButton
-                iconname={faHeart}
-                onClick={() => setWishListOpen(!wishListOpen)}
-                className="circle"
-            />
-            <IconButton iconname={faBell} className="circle"/>
-            <IconButton
-                iconname={faShoppingCart}
-                onClick={() => toggleCartOpen(!isCartOpen)}
-                className="circle"
-            />
-            <text className="cartCount" onClick={() => toggleCartOpen(!isCartOpen)}>
-                {cartItems.length}
-            </text>
-            {isCartOpen && <Cart newRef={searchResultDivRef}/>}
+  if (wishlist?.products?.length > 0) {
+    WishListItems = wishlist.products.map((product) => (
+      <WishlitItem id={product.id} title={product.title} />
+    ));
+  }
 
-            <Button
-                onClick={() => (user ? logout() : setModalOpen(true))}
-                className="team-btn"
-                radius={5}
-                bgColor="#8b49f4"
-                style={{width: "50%", padding: "1px"}}
-            >
-                {user ? "Logout (" + user.userName + ")" : "Sign In"}
-            </Button>
-            <SignInModal
-                style={{
-                    visibility: modalOpen ? "visible" : "hidden",
-                    opacity: modalOpen ? 1 : 0,
-                }}
-                setModalOpen={setModalOpen}
-            />
-            {wishListOpen && (
-                <div className="wishListDiv" ref={wishlistResultDivRef}>
-                    {WishListItems.length < 1 ? "No Items In Wishlist" : WishListItems}
-                </div>
-            )}
-        </>
-    );
+  return (
+    <>
+      <IconButton
+        iconname={faSun}
+        className="circle bgOrange hover"
+        onClick={() => changeTheme()}
+      />
+      <IconButton
+        iconname={faHeart}
+        onClick={() => setWishListOpen(!wishListOpen)}
+        className="circle"
+      />
+      <IconButton iconname={faBell} className="circle" />
+      <IconButton
+        iconname={faShoppingCart}
+        onClick={() => toggleCartOpen(!isCartOpen)}
+        className="circle"
+      />
+      <text className="cartCount" onClick={() => toggleCartOpen(!isCartOpen)}>
+        {cartItems.length}
+      </text>
+      {isCartOpen && <Cart newRef={searchResultDivRef} />}
+
+      <Button
+        onClick={() => (user ? logout() : setModalOpen(true))}
+        className="team-btn"
+        radius={5}
+        bgColor="#8b49f4"
+        style={{ width: "50%", padding: "1px" }}
+      >
+        {user ? "Logout (" + user.userName + ")" : "Sign In"}
+      </Button>
+      <SignInModal
+        style={{
+          visibility: modalOpen ? "visible" : "hidden",
+          opacity: modalOpen ? 1 : 0,
+        }}
+        setModalOpen={setModalOpen}
+      />
+      {wishListOpen && (
+        <div className="wishListDiv" ref={wishlistResultDivRef}>
+          {!user
+            ? "You Need To Login"
+            : WishListItems.length < 1
+            ? "No Items In Wishlist"
+            : WishListItems}
+        </div>
+      )}
+    </>
+  );
 }
