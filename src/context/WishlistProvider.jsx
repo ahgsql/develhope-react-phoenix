569a6870576d5e25e2c60f96061ca6b340416287
AlptekinOcakdan <alptekin.ocakdan@gmail.com>
2023-09-13 22:53:30 +0300

    Wishlist Provider have been handled.

--- src/context/WishlistProvider.jsx
+++ src/context/WishlistProvider.jsx
@@ -1,72 +1,90 @@
-import { createContext, useContext, useState, useEffect } from "react";
-import { useAuth } from "./AuthProvider.jsx";
+import {
+    createContext,
+    useContext,
+    useState,
+    useEffect,
+} from "react";
+import {useAuth} from "./AuthProvider.jsx";
 import axios from "axios";
-import {toast} from "react-toastify";
+import getWishlist from "../hooks/getWishlist.js";
 
 const WishlistContext = createContext();
 
+// Custom hook to access the WishlistContext
 export function useWishlist() {
     return useContext(WishlistContext);
 }
 
-export function WishlistProvider({ children }) {
-    const { user } = useAuth();
+export function WishlistProvider({children}) {
+    const {user} = useAuth();
     const [wishlist, setWishlist] = useState([]);
     const [number, setNumber] = useState(0);
 
     const addProductToWishlist = async (productId) => {
         try {
-            if (!user) return;
-            const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/wishlist`, {
-                productId,
-            });
-            if (response.data.status) {
-                toast.success(response.data.message);
-                setWishlist(response.data.wishlist);
+            if (!user) {
+                return console.log("User not logged in.");
             }
-            setNumber(number+1);
+
+            const response = await axios.post(
+                import.meta.env.VITE_BASE_URL + "/api/wishlist",
+                {productId},
+                {withCredentials: true}
+            );
+
+            if (response.data.status === true) {
+                setWishlist(response.data.wishlist);
+                console.log("Product added to wishlist");
+            }
+            setNumber(number + 1);
         } catch (error) {
-            console.error("Error adding product to wishlist:", error);
+            console.error(error);
         }
     };
 
     const removeProductFromWishlist = async (productId) => {
         try {
-            if (!user) return;
-            const response = await axios.delete(
-                `${import.meta.env.VITE_BASE_URL}/api/wishlist/${productId}`
-            );
-            if (response.data) {
-                toast.success("Product removed from wishlist");
-                setWishlist(response.data);
+            if (!user) {
+                return console.log("User not logged in.");
             }
-            setNumber(number-1);
+
+            const response = await axios.delete(
+                import.meta.env.VITE_BASE_URL + `/api/wishlist/${productId}`,
+                {withCredentials: true}
+            );
+
+            if (response.data) {
+                setWishlist(response.data);
+                console.log("Product removed from wishlist");
+            }
+            setNumber(number - 1);
         } catch (error) {
-            console.error("Error removing product from wishlist:", error);
+            console.error(error);
         }
     };
 
     useEffect(() => {
-        if (user) {
-            const fetchWishlist = async () => {
-                try {
-                    const response = await axios.get(
-                        `${import.meta.env.VITE_BASE_URL}/api/wishlist`
-                    );
-                    if (response.data.success) {
-                        setWishlist(response.data.wishlist);
-                    }
-                } catch (error) {
-                    console.error("Error fetching wishlist:", error);
-                }
-            };
-            fetchWishlist();
-        } else {
-            setWishlist([]);
-        }
+        (async () => {
+            if (!user) {
+                setWishlist([]);
+                return;
+            }
+
+            const wishlistFromDb = await getWishlist();
+            if (wishlistFromDb) {
+                setWishlist(wishlistFromDb);
+            }
+        })();
     }, [user,number]);
+
     return (
-        <WishlistContext.Provider value={{wishlist, addProductToWishlist, removeProductFromWishlist,}}>
+        <WishlistContext.Provider
+            value={{
+                wishlist,
+                addProductToWishlist,
+                removeProductFromWishlist,
+            }}
+        >
             {children}
         </WishlistContext.Provider>
     );
