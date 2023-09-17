c79b9e200a6bb39aad4cbf375c8348dc9a5f3164
ahgsql <35695543+ahgsql@users.noreply.github.com>
2023-09-17 22:52:27 +0300

    Fix Category Hiding, added default photo for non working photos

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -56,7 +56,14 @@ const ProductCard = ({
       </button>
       <Link to={"/product/" + slug} style={{ textDecoration: "none" }}>
         <div className="productImage">
-          <img src={photoFull} alt="" />
+          <img
+            src={photoFull}
+            onError={(e) => {
+              e.target.src =
+                "https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081";
+            }}
+            alt=""
+          />
         </div>
         <div className="productDescription">
           <h4 className="productName">
